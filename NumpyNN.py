02419569d31dd8010eb761d3323a67a2c4acef5b
George Chiriluta <george.chiriluta@student.manchester.ac.uk>
2019-06-14 00:47:09 +0300

    @@

--- NumpyNN.py
+++ NumpyNN.py
@@ -1,34 +1,38 @@
 import numpy as np
+import torch
+
+device = torch.device("cpu")
+# device = torch.device("cuda:0") # uncomment this line to run on GPU
 
 # N is the batch size, D_in is the input dimension, H is the hidden dimension, D_out is the output dimension
 N, D_in, H, D_out = 64, 1000, 100, 10
 
 # create random input and output data
-x = np.random.randn(N, D_in)
-y = np.random.randn(N, D_out)
+x = torch.randn(N, D_in, device=device, dtype=torch.float)
+y = torch.randn(N, D_out, device=device, dtype=torch.float)
 
 # randomly initialize weights
-w1 = np.random.randn(D_in, H)
-w2 = np.random.randn(H, D_out)
+w1 = torch.randn(D_in, H, device=device, dtype=torch.float)
+w2 = torch.randn(H, D_out, device=device, dtype=torch.float)
 
 learning_rate = 1e-6
 for t in range(500):
     # forward pass: compute predicted y
-    h = x.dot(w1)
-    h_relu = np.maximum(h, 0)
-    y_pred = h_relu.dot(w2)
+    h = x.mm(w1)
+    h_relu = h.clamp(min=0)
+    y_pred = h_relu.mm(w2)
 
     # compute and print loss
-    loss = np.square(y_pred - y).sum()
+    loss = (y_pred - y).pow(2).sum().item()
     print(t, loss)
 
     # backprop to compute gradients of w1 and w2 with respect to loss
     grad_y_pred = 2.0 * (y_pred - y)
-    grad_w2 = h_relu.T.dot(grad_y_pred)
-    grad_h_relu = grad_y_pred.dot(w2.T)
-    grad_h = grad_h_relu.copy()
+    grad_w2 = h_relu.t().mm(grad_y_pred)
+    grad_h_relu = grad_y_pred.mm(w2.t())
+    grad_h = grad_h_relu.clone()
     grad_h[h < 0] = 0
-    grad_w1 = x.T.dot(grad_h)
+    grad_w1 = x.t().mm(grad_h)
 
     # Update weights
     w1 -= learning_rate * grad_w1
